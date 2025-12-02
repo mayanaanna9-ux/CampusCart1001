484e2f4080be0fa16609e7329f65d79670c5bb16
Reki Kianne <rekikianne@gmail.com>
2025-12-02 05:28:52 +0000

    use the google icon which is colorful (_for element <Button>_)

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -203,7 +203,7 @@ export function LoginForm() {
         </div>
         <div className="space-y-2">
             <Button variant="outline" className="w-full font-bold" size="lg" onClick={onGoogleSignIn}>
-                <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 381.5 512 244 512 109.8 512 0 402.2 0 256S109.8 0 244 0c73 0 135.7 28.7 182.4 73.5l-65.5 63.5C334.4 111.9 292.1 94.5 244 94.5c-83.3 0-151.7 68.2-151.7 152.1s68.4 152.1 151.7 152.1c96.5 0 131.2-69.5 136-104.3H244v-73.6h236.1c2.4 12.7 3.9 26.5 3.9 41.6z"></path></svg>
+                <svg className="mr-2 h-4 w-4" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><g><path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path><path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path><path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path><path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path><path fill="none" d="M0 0h48v48H0z"></path></g></svg>
                 Google
             </Button>
             <Button variant="outline" className="w-full font-bold" size="lg" onClick={onGuestLogin}>Continue as Guest</Button>
