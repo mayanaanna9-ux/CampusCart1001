630d4a1b368ac5fc07e65eea3afbfba688e50b64
Reki Kianne <rekikianne@gmail.com>
2025-12-15 04:40:20 +0000

    Add another paragraph beneath this "the author of this app can be contac

--- src/app/(app)/about/page.tsx
+++ src/app/(app)/about/page.tsx
@@ -24,6 +24,9 @@ export default function AboutPage() {
           <p className="text-center text-muted-foreground">
             The owner of this app is from a BSIT-CPT03 student named Phoebe Ayana A. Andal, this app promotes a chance for students to online post their items that they wanted to sell for other students and limit their time thinking where to promote those item as they can easily sell items where students can easily access.
           </p>
+          <p className="text-center text-muted-foreground mt-4">
+            the author of this app can be contacted through email, Facebook or by phone number. Your opinions and support is greatly appreciated and all of this is for everyone in campus to use freely. Once again thank you!
+          </p>
         </CardContent>
       </Card>
     </div>
